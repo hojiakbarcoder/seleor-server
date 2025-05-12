9a0515b3179935e6817f017fee7c3904257e8ba0
Hojiakbar Habibullayev <hojiakbarhabibullayev@Hojiakbarui-MacBookPro.local>
2025-05-12 21:32:52 +0900

    otp-email-service

--- routes/otp.js
+++ routes/otp.js
@@ -1,7 +1,8 @@
+const otpController = require('../controllers/otp.controller')
+
 const router = require('express').Router()
 
-router.post('/send-otp', (req, res, next) => {})
-router.post('/verify-otp', (req, res, next) => {})
-router.post('/resend-otp', (req, res, next) => {})
+router.post('/send', otpController.sendOtp)
+router.post('/verify', otpController.verifyOtp)
 
 module.exports = router

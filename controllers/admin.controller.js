f70da84930f1deed4de5c2bc93aacdb21f0ea847
Hojiakbar Habibullayev <hojiakbarhabibullayev@Hojiakbarui-MacBookPro.local>
2025-05-19 20:01:43 +0900

    pagination|filter

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -8,7 +8,6 @@ class AdminController {
 		this.userId = '68272cbfe44de24593a3eed1'
 		this.createProduct = this.createProduct.bind(this)
 		this.updateProduct = this.updateProduct.bind(this)
-		this.getProducts = this.getProducts.bind(this)
 		this.deleteProduct = this.deleteProduct.bind(this)
 		this.getCustomers = this.getCustomers.bind(this)
 		this.getOrders = this.getOrders.bind(this)
@@ -19,8 +18,34 @@ class AdminController {
 	//[GET] /admin/products
 	async getProducts(req, res, next) {
 		try {
-			const products = await productModel.find()
-			return res.json({ products })
+			const { searchQuery, filter, category, page, pageSize } = req.query
+			const skipAmount = (+page - 1) * +pageSize
+			const query = {}
+
+			if (searchQuery) {
+				const escapedSearchQuery = searchQuery.replace(
+					/[.*?^${}()|[\]\\]/g,
+					'\\$&'
+				)
+				query.$or = [{ title: { $regex: new RegExp(escapedSearchQuery, 'i') } }]
+			}
+			if (category === 'All') query.category = { $exists: true }
+			else if (category !== 'All') {
+				if (category) query.category = category
+			}
+			let sortOptions = { createdAt: -1 }
+			if (filter === 'newest') sortOptions = { createdAt: -1 }
+			else if (filter === 'oldest') sortOptions = { createdAt: 1 }
+
+			const products = await productModel
+				.find(query)
+				.sort(sortOptions)
+				.skip(skipAmount)
+				.limit(+pageSize)
+			const totalProducts = await productModel.countDocuments(query)
+			const isNext = totalProducts > skipAmount + +products.length
+			console.log(isNext)
+			return res.json({ products, isNext })
 		} catch (error) {
 			next(error)
 		}
@@ -90,15 +115,10 @@ class AdminController {
 		try {
 			const data = req.body
 			const { id } = req.params
-			const userId = this.userId
-			const user = await userModel.findById(userId)
-			if (!user) return res.json({ failure: 'User not found' })
-			if (user.role !== 'admin')
-				return res.json({ failure: 'User is not Admin' })
 			const updatedProduct = await productModel.findByIdAndUpdate(id, data)
 			if (!updatedProduct)
 				return res.json({ failure: 'Failed while updating product' })
-			return res.json({ success: 'Product updated successfully' })
+			return res.json({ status: 200 })
 		} catch (error) {
 			next(error)
 		}
@@ -126,15 +146,10 @@ class AdminController {
 	async deleteProduct(req, res, next) {
 		try {
 			const { id } = req.params
-			const userId = this.userId
-			const user = await userModel.findById(userId)
-			if (!user) return res.json({ failure: 'User not found' })
-			if (user.role !== 'admin')
-				return res.json({ failure: 'User is not Admin' })
 			const deleteProduct = await productModel.findByIdAndDelete(id)
 			if (!deleteProduct)
 				return res.json({ failure: 'Failed while deleting product' })
-			return res.json({ success: 'Product deleted successfully' })
+			return res.json({ status: 200 })
 		} catch (error) {
 			next(error)
 		}
